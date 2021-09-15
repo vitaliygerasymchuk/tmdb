41aff358e25fff137375465019ad9bca269d67fb
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2021-09-15 10:16:30 +0300

    abs initialization

---
+++ app/src/main/java/app/tmdb/test/ui/AbsFragment.kt
@@ -0,0 +1,24 @@
+package app.tmdb.test.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class AbsFragment<V : ViewBinding> : Fragment() {
+
+    protected lateinit var binding: V
+
+    abstract fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?): V
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = inflateBinding(inflater, container)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/app/tmdb/test/ui/auth/LoginFragment.kt
@@ -0,0 +1,28 @@
+package app.tmdb.test.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.tmdb.test.databinding.FragmentLoginBinding
+import app.tmdb.test.ui.AbsFragment
+
+class LoginFragment : AbsFragment<FragmentLoginBinding>() {
+
+    override fun inflateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentLoginBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initBinding() {
+        with(binding) {
+            textView
+        }
+    }
+
+    private fun observeViewModel() {}
+}
