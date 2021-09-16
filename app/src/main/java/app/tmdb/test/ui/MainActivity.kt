0b5649e31e953d324f7bf9242120fed8b40d011e
Tendai <59752146+tendaimusakanye@users.noreply.github.com>
2021-09-16 15:17:31 +0200

    Move MainActivity.kt

--- app/src/main/java/app/tmdb/test/ui/MainActivity.kt
+++ app/src/main/java/app/tmdb/test/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package app.tmdb.test
+package app.tmdb.test.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.tmdb.test.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
