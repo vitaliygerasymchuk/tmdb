2b7ba0fb83eaef1859d26aba1a3ec75da1d13659
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2021-09-20 10:56:15 +0300

    permission example

--- app/src/main/java/app/tmdb/test/ui/login/LoginFragment.kt
+++ app/src/main/java/app/tmdb/test/ui/login/LoginFragment.kt
@@ -1,22 +1,31 @@
 package app.tmdb.test.ui.login
 
-
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import app.tmdb.test.databinding.FragmentLoginBinding
 import app.tmdb.test.ui.AbsFragment
+import app.tmdb.test.utils.Loggable
+import app.tmdb.test.utils.hasPermission
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LoginFragment : AbsFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+class LoginFragment : AbsFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate), Loggable {
 
     private val viewModel: LoginViewModel by viewModels()
+
+    private val locationPermissionCall =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            //recursion
+            log("onPermissionResult received")
+        }
 
     override fun setUp() {
         with(binding) {
@@ -30,7 +39,25 @@ class LoginFragment : AbsFragment<FragmentLoginBinding>(FragmentLoginBinding::in
                 viewModel.loginAsGuest()
             }
         }
+
+        askLocationPermission()
     }
+
+    private fun askLocationPermission() {
+        when {
+            hasPermission(Manifest.permission.CAMERA) -> {
+                takePicture()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                // showDialog and try again
+            }
+            else -> {
+                locationPermissionCall.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private fun takePicture() {}
 
     @SuppressLint("QueryPermissionsNeeded")
     override fun setUpObservers() {
@@ -46,8 +73,10 @@ class LoginFragment : AbsFragment<FragmentLoginBinding>(FragmentLoginBinding::in
 
         viewModel.requestToken.observe(viewLifecycleOwner, { token ->
             binding.progressBar.visibility = View.GONE
-            val intent = Intent(Intent.ACTION_VIEW,
-                Uri.parse("https://www.themoviedb.org/authenticate/$token"))
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.themoviedb.org/authenticate/$token")
+            )
             try {
                 startActivity(intent)
             } catch (ex: ActivityNotFoundException) {
--- app/src/main/java/app/tmdb/test/utils/Fragment.kt
+++ app/src/main/java/app/tmdb/test/utils/Fragment.kt
@@ -1,9 +1,19 @@
 package app.tmdb.test.utils
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 
 fun Fragment.navigate(resId: Int) {
     findNavController().navigate(resId)
+}
+
+fun Fragment.hasPermission(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        requireContext(),
+        permission
+    ) == PackageManager.PERMISSION_GRANTED
 }
