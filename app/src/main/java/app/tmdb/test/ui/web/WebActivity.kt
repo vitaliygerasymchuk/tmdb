4b8eaeb2be488d460966e542fbb5c312a50d8eaa
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2021-09-20 10:22:10 +0300

    web view.

---
+++ app/src/main/java/app/tmdb/test/ui/web/WebActivity.kt
@@ -0,0 +1,12 @@
+package app.tmdb.test.ui.web
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.tmdb.test.R
+
+class WebActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+    }
+}
