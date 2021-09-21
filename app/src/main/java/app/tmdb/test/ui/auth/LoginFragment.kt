e45535088b2fdbbbf529635083af2650bdbc1521
Tendai <59752146+tendaimusakanye@users.noreply.github.com>
2021-09-21 20:36:00 +0200

    Refactor LoginFragment.kt

--- app/src/main/java/app/tmdb/test/ui/auth/LoginFragment.kt
+++ app/src/main/java/app/tmdb/test/ui/auth/LoginFragment.kt
@@ -1,8 +1,8 @@
 package app.tmdb.test.ui.auth
 
-import android.R
 import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
+import app.tmdb.test.R
 import app.tmdb.test.databinding.FragmentLoginBinding
 import app.tmdb.test.ui.AbsFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,19 +13,21 @@ class LoginFragment : AbsFragment<FragmentLoginBinding>(FragmentLoginBinding::in
     private val viewModel: LoginViewModel by viewModels()
 
     override fun setUp() {
-        with(binding) {
-            val adapter = ArrayAdapter(requireContext(),
-                android.R.layout.simple_dropdown_item_1line,
-                resources.getStringArray(app.tmdb.test.R.array.api_keys))
-
-            apiKeyAutocomplete.setAdapter(adapter)
-            apiKeyAutocomplete.setOnItemClickListener { _, _, position, _ ->
-                // Do some stuff
-            }
-        }
+        binding.setUpApiKeyAutoComplete()
     }
 
     override fun setUpObservers() {
         TODO("Not yet implemented")
+    }
+
+    private fun FragmentLoginBinding.setUpApiKeyAutoComplete() {
+        val adapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_dropdown_item_1line,
+            resources.getStringArray(R.array.api_keys))
+
+        apiKeyAutocomplete.setAdapter(adapter)
+        apiKeyAutocomplete.setOnItemClickListener { _, _, position, _ ->
+            // Do some stuff
+        }
     }
 }
